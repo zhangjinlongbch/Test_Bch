2457b333ffc8965332cccb23ec30d1e91511fc11
BCH_ZhangJinlong <1050483798@qq.com>
2015-09-01 17:40:20 +0800

    hehe ~~~

---
+++ Bch_Test/Bch_Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bch_Test.MvcApplication" Language="C#" %>
---
+++ Bch_Test/Bch_Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bch_Test.Startup))]
+namespace Bch_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
