1462c4e9f20fafba04d40beaa6ef583fd6228003
ZhangJinlong <1050483798@qq.com>
2015-09-01 21:34:10 +0800

    提交了.NET FrameWork+VS2013版本，文件名为“BCH_MVC_VS2013”

---
+++ BCH_MVC_VS2013/BCH_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BCH_MVC.MvcApplication" Language="C#" %>
---
+++ BCH_MVC_VS2013/BCH_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BCH_MVC.Startup))]
+namespace BCH_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
